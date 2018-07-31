ea31d3f4009262478db445d2226c97a2db70ef73
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-07-31 22:14:52 +0530

    Update PetProvider.java

--- data/PetProvider.java
+++ data/PetProvider.java
@@ -1,43 +1,155 @@
-package com.example.android.pets.data;
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.pets;
 
-import android.content.ContentProvider;
+import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
-import android.net.Uri;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.android.pets.data.PetDbHelper;
+import com.example.android.pets.data.PetContract.PetEntry;
 
 /**
- * Created by Puspak Biswas on 26-07-2018.
+ * Displays list of pets that were entered and stored in the app.
  */
+public class CatalogActivity extends AppCompatActivity {
 
-public class PetProvider extends ContentProvider {
+    private PetDbHelper mDbHelper;
 
-    private PetDbHelper mPetDbHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+        Log.i("CatalogActivity", "onCreate: Has been called ");
 
-    public static final String LOG_TAG = PetContract.PetEntry.class.getSimpleName();
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+        mDbHelper = new PetDbHelper(this);
+        displayDatabaseInfo();
+    }
 
-    public boolean onCreate(){
-        mPetDbHelper = new PetDbHelper(getContext());
+    protected void onStart(){
+        super.onStart();
+        displayDatabaseInfo();
+    }
+
+    private void displayDatabaseInfo() {
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+
+        String[] projection = {PetEntry._ID,PetEntry.COLUMN_PET_NAME,PetEntry.COLUMN_PET_BREED,PetEntry.COLUMN_PET_GENDER,
+        PetEntry.COLUMN_PET_WEIGHT};
+
+        Cursor cursor = getContentResolver().query(PetEntry.CONTENT_URI,projection,null,null,null);
+        TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+
+        try {
+            // Create a header in the Text View that looks like this:
+            //
+            // The pets table contains <number of rows in Cursor> pets.
+            // _id - name - breed - gender - weight
+            //
+            // In the while loop below, iterate through the rows of the cursor and display
+            // the information from each column in this order.
+            displayView.setText("The pets table contains " + cursor.getCount() + " pets.\n\n");
+            displayView.append(PetEntry._ID + " - " +
+                    PetEntry.COLUMN_PET_NAME + "-" + PetEntry.COLUMN_PET_BREED + "-" +
+                    PetEntry.COLUMN_PET_GENDER + "-" + PetEntry.COLUMN_PET_WEIGHT + "\n");
+
+            // Figure out the index of each column
+            int idColumnIndex = cursor.getColumnIndex(PetEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
+            int breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
+            int genderColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_GENDER);
+            int weightColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT);
+
+            // Iterate through all the returned rows in the cursor
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentName = cursor.getString(nameColumnIndex);
+                String currentBreed = cursor.getString(breedColumnIndex);
+                int currentGender = cursor.getInt(genderColumnIndex);
+                int currentWeight = cursor.getInt(weightColumnIndex);
+                // Display the values from each column of the current row in the cursor in the TextView
+                displayView.append(("\n" + currentID + " - " +
+                        currentName + "-" + currentBreed + "-" + currentGender + "-" + currentWeight));
+            }
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
         return true;
     }
 
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
-                        String sortOrder){
-        return null;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                insertRow();
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                // Do nothing for now
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
-    public Uri insert(Uri uri, ContentValues contentValues){
-        return null;
-    }
+    public void insertRow(){
+        ContentValues values = new ContentValues();
+        values.put(PetEntry.COLUMN_PET_NAME,"Toto");
+        values.put(PetEntry.COLUMN_PET_BREED,"Terrier");
+        values.put(PetEntry.COLUMN_PET_GENDER,1);
+        values.put(PetEntry.COLUMN_PET_WEIGHT,14);
 
-    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs){
-        return 0;
-    }
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        long returnOfInsert = db.insert(PetEntry.TABLE_NAME,null,values);
 
-    public int delete(Uri uri, String selection, String[] selectionArgs){
-        return 0;
-    }
-
-    public String getType(Uri uri){
-        return null;
+        if(returnOfInsert == -1){
+            Log.e("InsertResult","not correct");
+        }
+        displayDatabaseInfo();
     }
 }
