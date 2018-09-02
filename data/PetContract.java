391c4cebb2e202305ceea69ad1fd4cb6f7dfb428
puspak-biswas <38313124+puspak-biswas@users.noreply.github.com>
2018-09-02 18:35:11 +0530

    Update PetContract.java

--- data/PetContract.java
+++ data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -17,6 +18,16 @@ public final class PetContract {
 
     public static final class PetEntry implements BaseColumns{
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI,PATH_PETS);
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of pets.
+         */
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE+"/"+
+                CONTENT_AUTHORITY+"/"+PATH_PETS;
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE+"/"+
+                CONTENT_AUTHORITY+"/"+PATH_PETS;
         public static final String TABLE_NAME = "pets";
         public static final String _ID = BaseColumns._ID;
         public static final String COLUMN_PET_NAME = "name";
